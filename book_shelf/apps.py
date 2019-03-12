fbd9c5cc72fa43b259e19c0bca8ecf834f3b51f9
Kasey Tiller <kntiller@ccicustom.com>
2019-03-12 15:10:42 -0400

    Deleting everything and starting over.

--- book_shelf/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookShelfConfig(AppConfig):
-    name = 'book_shelf'
--- book_shelf/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from book_shelf.models import BookShelf, Book, BookCategory, BookAuthor
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
