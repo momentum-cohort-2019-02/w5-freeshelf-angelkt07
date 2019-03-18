a0ea965b9ae6e09057c67cf3e17dbaa8d8883707
Kasey Tiller <kntiller@ccicustom.com>
2019-03-17 22:32:51 -0400

    Tried favorites; removed; pushing code without favorites

--- book_shelf/urls.py
+++ book_shelf/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from book_shelf import views
 
 urlpatterns = [
     path('', views.index, name='index'),
--- book_shelf/views.py
+++ book_shelf/views.py
@@ -1,13 +1,14 @@
 import datetime
-from django.shortcuts import render
-from book_shelf.models import Book, Category, Author
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import permission_required, login_required
+from django.urls import reverse, reverse_lazy
+from django.views.decorators.http import require_http_methods
+
+
+from book_shelf.models import Book, Category, Author
 
 # Create your views here.
 
@@ -84,6 +85,21 @@ class AuthorDelete(DeleteView):
 class CategoryDelete(DeleteView):
     model = Category
     success_url = reverse_lazy('categories')
+
+
+# def favorite_book_view(request, book_pk):
+#     book = get_object_or_404(Book, pk=book_pk)
+
+#     favorite, created = request.user.favorite_set.get_or_create(book=book)
+
+#     if created:
+#         return
+#     else:
+#         favorite.delete()
+
+#     return redirect(book.get_absolute_url())
+
+
 
 
 
